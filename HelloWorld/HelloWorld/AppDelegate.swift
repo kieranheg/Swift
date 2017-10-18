d255715acf533617b09d3dde1a77fed2ec150e00
Kieran Hegarty <kieranheg@users.noreply.github.com>
2017-10-18 19:16:22 +0100

    Add HelloWorld

---
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HelloWorld
+//
+//  Created by Kieran Hegarty on 07/10/2017.
+//  Copyright © 2017 Kieran Hegarty. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,30 @@
+
+import Cocoa
+
+class ViewController: NSViewController {
+    @IBOutlet weak var nameField: NSTextField!
+    @IBOutlet weak var helloLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+            name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
+    
+}
+
